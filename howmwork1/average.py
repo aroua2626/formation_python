b48c1116b1d0a2e9450b3268a2eb3507e463e017
aroua2626 <a_mellit@estin.dz>
2025-06-13 14:33:26 +0100

    Update average.py

--- howmwork1/average.py
+++ howmwork1/average.py
@@ -1,1 +1,7 @@
-
+s1 = float(input("enter the first semester average: "))
+s2 = float(input("enter the second semester average: "))
+s = s1+s2
+if s >= 20:
+   print("valid")
+else:
+   print("non valid")   

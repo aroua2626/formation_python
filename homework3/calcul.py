c3c849e684d29902408e37de8e980749f10b0eb2
aroua2626 <a_mellit@estin.dz>
2025-06-17 14:10:45 +0100

    Update calcul.py

--- homework3/calcul.py
+++ homework3/calcul.py
@@ -1,1 +1,17 @@
-
+a = float(input("enter the first number:"))
+b = float(input("enter the second number:"))
+import random
+option = random.choice(['+', '-', '*', '/'])
+print(option)
+if option == '+':
+    print("the addition",a+b)
+elif option is '-':
+    print("the substruction",a-b)
+elif option == '*':
+    print("the multiplication",a*b)
+elif option is '/':
+    if b!=0:
+     print("the division",a/b)
+    else:
+     print("error:zerodivision") 
+    

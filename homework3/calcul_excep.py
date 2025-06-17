3c66b439ffcecc1a36629b3b4673d2c4d4737776
aroua2626 <a_mellit@estin.dz>
2025-06-17 14:12:35 +0100

    Create calcul_excep.py

---
+++ homework3/calcul_excep.py
@@ -0,0 +1,16 @@
+a = float(input("enter the first number:"))
+b = float(input("enter the second number:"))
+import random
+option = random.choice(['+', '-', '*', '/'])
+print(option)
+if option == '+':
+    print("the addition",a+b)
+elif option is '-':
+    print("the substruction",a-b)
+elif option == '*':
+    print("the multiplication",a*b)
+elif option is '/':
+    try:
+    print("the division",a/b)
+    except Exception as {ZeroDivisionError}
+                
